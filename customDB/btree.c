53b1f7b69a247f82c8182a200ae31f5f574fcffb
Austen Barker <atbarker@ucsc.edu>
2018-03-09 10:35:32 -0800

    woo

---
+++ customDB/btree.c
@@ -0,0 +1,34 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "btree.h"
+
+//search the btree for a specific key
+void *btree_search_node(){
+    return NULL;
+}
+
+//free a node in the btree
+int btree_free_node(){
+    return 0;
+}
+
+btree_node *btree_create_node(){
+
+}
+
+btree *btree_create(){
+
+}
+
+void *btree_search(){
+    return NULL;
+}
+
+int btree_free(){
+    return 0;
+}
+
+int btree_insert(){
+    return 0;
+}
--- customDB/database.c
+++ customDB/database.c
@@ -3,3 +3,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+//hash table implementation
+
+hash_t *tables;
+
+int init_db(char *newDB){
+    tables = hash_new();
+
+    return 0;
+}
+
+int create_table(char *new){
+
+    return 0;
+}
+
+int drop_table(Table *table){
+    if(!table){
+        printf("No such table\n");
+        return -1;
+    }
+    return 0;
+}
+
+int add_column(Table *table, char *new_column){
+    return 0;
+}
+
+int drop_column(Table *table, Column *column){
+    if(!column){
+        return -1;
+    }
+    return 0;
+}
+
+int drop_row(Table *table, Row *row){
+   return 0;
+}
+
+Column *find_column(){
+
+}
+
+int insert(){
+    return 0;
+}
+
+int select(){
+    return 0;
+}
+
+int list_columns(Table, *table){
+   return 0;
+}
+
+int list_tables(){
+   return 0;
+}
+
+int where(char *table_name, char *column_name, char *value){
+   return 0;
+}
