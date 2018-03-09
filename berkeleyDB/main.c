0f849c9439eb06755c5d859e63a5eb5cc25865f6
Austen Barker <austenbarker@yahoo.com>
2018-03-09 10:10:08 -0800

    Update main.c

---
+++ berkeleyDB/main.c
@@ -0,0 +1,18 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <db.h>
+
+/*
+ *argv[0] program name
+ *argv[1] time desired
+ *argv[2] DB file
+ *argv[3] new db file 
+ *argv[4] WAL file
+ */
+int main(int argc, char *argv[]){
+    return 0;
+}
