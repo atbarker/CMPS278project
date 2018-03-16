ea4d5f4026b13709b4bb9e3ab7505cdcbc9f07f0
Austen Barker <atbarker@ucsc.edu>
2018-03-15 21:41:17 -0700

    copied things into worker folder

--- berkeleyDB/rollback.c
+++ berkeleyDB/rollback.c
@@ -30,7 +30,90 @@ int key_exists(unsigned char *changed, int length, int key){
 //retrieve a batch of log records and crunch the numbers
 void* rollback_worker(void *args){
     struct thread_args *td_args = args;
-    	
+
+    /*DB_LOGC *cursor;
+    DB_LSN *last_lsn = malloc(sizeof(DB_LSN));
+    DBT *log_contents = malloc(sizeof(DBT) * 6);
+    struct db_log_record *log = NULL;
+    struct rollback_summary *sum = malloc(sizeof(struct rollback_summary));
+    int i;
+    int rollback_count = 0;
+
+    sum->diffs_length = context->number_keys;
+    printf("number of keys %u\n", sum->diffs_length);
+    sum->diffs = malloc(sum->diffs_length * sizeof(unsigned char *));
+    for(i = 0; i < sum->diffs_length; i++){
+        sum->diffs[i] = malloc(40);    
+    }
+    sum->changed = malloc(sum->diffs_length/8);
+
+    //create the cursor
+    if(env->log_cursor(env, &cursor, 0)){
+        fprintf(stderr, "Error creating database cursor\n");
+        return NULL;
+    }
+
+    //have to initialize the objects
+    memset(last_lsn, 0, sizeof(DB_LSN));
+    memset(log_contents, 0, sizeof(DBT));
+    log_contents->data = NULL;
+    log_contents->size = sizeof(struct db_log_record);
+ 
+    //scan from the most recent log record to the correct timestamp or LSN
+    //printf("grabbing first log record\n");
+    cursor->get(cursor, last_lsn, log_contents, DB_LAST);
+    log = log_contents->data;
+    //printf("timestamp: %lu \n", log->time);
+    printf("LSN: %u %u \n", last_lsn->file, last_lsn->offset);
+
+    //grab each log record starting from that LSN
+    for(i = 1; i < sum->diffs_length; i++){
+        //printf("Grabbing recent log %d\n", i);
+        cursor->get(cursor, last_lsn, log_contents, DB_PREV);
+        printf("LSN: %u %u \n", last_lsn->file, last_lsn->offset);
+        log = log_contents->data;
+        int ret;
+        //represent empty struct somehow
+        if(ret = key_exists(sum->changed, rollback_count, log->key)){
+            if(log->type == 0){
+                memcpy(sum->diffs[ret], log->data, sizeof(struct character));
+            }else if(log->type == 1){
+                continue;
+            }else if(log->type == 2){
+                unsigned char temp[sizeof(struct character)];
+                memcpy(temp, sum->diffs[ret], sizeof(struct character));
+                array_xor(log->data, temp, sum->diffs[ret], sizeof(struct character));
+            }else if(log->type == 3){
+                unsigned char temp[sizeof(struct character)];
+                memcpy(temp, sum->diffs[ret], sizeof(struct character));
+                array_xor(log->data, temp, sum->diffs[ret], sizeof(struct character));
+            }
+        }else{
+            if(log->type == 0){
+                memcpy(sum->diffs[rollback_count], log->data, sizeof(struct character));
+                bitmap_set(sum->changed, log->key, sum->diffs_length, 1);
+            }else if(log->type == 1){
+                continue;
+            }else if(log->type == 2){
+                unsigned char temp[sizeof(struct character)];
+                memcpy(temp, sum->diffs[rollback_count], sizeof(struct character));
+                array_xor(log->data, temp, sum->diffs[rollback_count], sizeof(struct character));
+                bitmap_set(sum->changed, log->key, sum->diffs_length, 1);
+            }else if(log->type == 3){
+                unsigned char temp[sizeof(struct character)];
+                memcpy(temp, sum->diffs[rollback_count], sizeof(struct character));
+                array_xor(log->data, temp, sum->diffs[rollback_count], sizeof(struct character));
+                bitmap_set(sum->changed, log->key, sum->diffs_length, 1);
+            }
+            rollback_count++;
+        }
+    }
+   
+    //cleanup
+    cursor->close(cursor, 0);
+    free(log_contents);
+    free(last_lsn);
+    return sum;*/
     return NULL;
 }
 
